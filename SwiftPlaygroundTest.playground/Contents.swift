a8c807fc2cd1c32961811243f94f87adf025b022
tafitzs <35237432+tafitzs@users.noreply.github.com>
2018-01-22 10:04:42 -1000

    strings

---
+++ SwiftPlaygroundTest.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var personname = "Tom"
+
+//string interpolation sample
+
+let greeting = "hi, my name is \(personname)"
